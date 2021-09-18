82f4029dcad093bd4b1e9e0ad98cab7960d69e74
jaime castano <jaime.castano@correo.tdea.edu.co>
2021-09-18 14:49:24 -0500

    configuracion inicial

--- src/main/java/net/software/backendcursojava/models/responses/UserRest.java
+++ src/main/java/net/software/backendcursojava/models/responses/UserRest.java
@@ -2,13 +2,20 @@ package net.software.backendcursojava.models.responses;
 
 public class UserRest {
 
-private String userId;
+    private String userId;
     private String firstName;
     private String lastName;
     private String email;
     
 
     
+    
+    public String getUserId() {
+        return userId;
+    }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
     public String getFirstName() {
         return firstName;
     }
